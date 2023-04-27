804b850e422da024c10343cbcbcc35dcc8adae9e
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-27 13:20:41 +0000

    refactor: remove cn and use only tailwindMerge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import { twMerge as tm } from "tailwind-merge"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
-import { cn } from "@/lib/utils"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
@@ -34,7 +34,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <html lang="en" suppressHydrationWarning>
         <head />
         <body
-          className={cn(
+          className={tm(
             "min-h-screen bg-background font-sans antialiased",
             fontSans.variable
           )}
