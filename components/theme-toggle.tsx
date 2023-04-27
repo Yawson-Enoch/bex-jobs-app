64f5537f3c9b5ce7d8de43c2d563cc8d41184ccb
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-27 15:06:49 +0000

    feat: update toggle position

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -24,7 +24,7 @@ export function ThemeToggle() {
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="center">
         <DropdownMenuItem onClick={() => setTheme('light')}>
           <Sun className="mr-2 h-4 w-4" />
           <span>Light</span>
