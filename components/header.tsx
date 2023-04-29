e9ea8afaaea414916572676e0d438acc9bef7217
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-29 23:01:47 +0000

    refactor: only add blur bg to container

--- components/header.tsx
+++ components/header.tsx
@@ -3,8 +3,8 @@ import { ThemeToggle } from './theme-toggle';
 
 export default function Header() {
   return (
-    <header className="bg-background/10 px-2 pt-2 backdrop-blur-sm">
-      <div className="container flex items-center justify-between gap-3 rounded-full border py-2 md:gap-5">
+    <header className="px-2 pt-2">
+      <div className="container flex items-center justify-between gap-3 rounded-full border bg-background/10 py-2 backdrop-blur-sm md:gap-5">
         <GradientLogo />
         <ThemeToggle />
       </div>
