2f2192e48486a73dea76d201c92542274b71c8eb
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-29 23:22:53 +0000

    refactor: create blobs component

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { twMerge } from 'tailwind-merge';
 
 import { siteConfig } from '@/config/site';
 import { fontSans } from '@/lib/fonts';
+import DecorativeBlobs from '@/components/decorative-blobs';
 import Footer from '@/components/footer';
 import Header from '@/components/header';
 import { TailwindIndicator } from '@/components/tailwind-indicator';
@@ -79,9 +80,9 @@ export default function RootLayout({
             {children}
             <Footer />
           </div>
+          <DecorativeBlobs />
           <TailwindIndicator />
         </ThemeProvider>
-        <div className="fixed inset-0 -z-10 overflow-hidden before:absolute before:bottom-5 before:left-5 before:h-56 before:w-56 before:rounded-[30%_70%_79%_21%_/_30%_44%_56%_70%] before:bg-gradient-to-b before:from-blue-500/20 before:from-40% before:to-yellow-500/10 before:to-95% before:blur-3xl before:content-[''] after:absolute after:right-5 after:top-5 after:h-56 after:w-56 after:rounded-[30%_70%_79%_21%_/_30%_44%_56%_70%] after:bg-gradient-to-b after:from-blue-500/20 after:from-40% after:to-yellow-500/10 after:to-95% after:blur-3xl after:content-[''] before:lg:h-96 before:lg:w-96 after:lg:h-96 after:lg:w-96"></div>
       </body>
     </html>
   );
