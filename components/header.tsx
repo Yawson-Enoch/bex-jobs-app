cc0dcb5cafa6476189e9f3573c538e361602682e
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-30 23:37:56 +0000

    feat: add link to home on logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import LoginBtn from './LoginBtn';
 import SignUpBtn from './SignUpBtn';
 import GradientLogo from './gradient-logo';
@@ -7,7 +9,9 @@ export default function Header() {
   return (
     <header className="px-2 pt-2">
       <div className="container flex items-center justify-between gap-3 rounded-full border bg-background/10 px-4 py-2 backdrop-blur-sm md:gap-5">
-        <GradientLogo />
+        <Link href="/">
+          <GradientLogo />
+        </Link>
         <div className="flex items-center gap-1 md:gap-10">
           <ThemeToggle />
           <div className="flex items-center gap-3">
