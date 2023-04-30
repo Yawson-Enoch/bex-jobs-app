cedcee947d0bf638e0759932bff683fb88fd124b
Yawson-Enoch <enochgybex25@gmail.com>
2023-04-30 11:46:02 +0000

    fix: tweak copy symbol alignment

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,10 @@
 export default function Footer() {
   return (
     <footer className="border-t bg-background/10 backdrop-blur-sm">
-      <div className="container flex items-center justify-center py-5">
-        <p>&copy; {new Date().getFullYear()} GyBex</p>
+      <div className="container flex items-center justify-center gap-2 py-3">
+        <p className="pt-[3px] text-lg">&copy;</p>
+        <p>{new Date().getFullYear()}</p>
+        <p>GyBex</p>
       </div>
     </footer>
   );
