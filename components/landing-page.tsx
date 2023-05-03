96f0f1b654b1d8218f999b86e292d43f3a3e0aae
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-03 11:52:30 +0000

    feat: add initial scaling to heading container

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -83,20 +83,23 @@ const mainBtn = {
   animate: {
     opacity: 1,
     y: 0,
-    transition: { duration: 0.3, ease: 'easeOut' },
+    transition: { duration: 0.5, ease: 'easeOut' },
   },
 };
 
 const headingContainer = (isMobile: boolean): Variants => ({
   initial: {
     opacity: 0,
+    scale: 0.25,
   },
   animate: {
     opacity: 1,
+    scale: 1,
     transition: {
       when: 'beforeChildren',
       staggerChildren: 0.25,
       staggerDirection: isMobile ? 1 : -1,
+      transition: { duration: 0.3, ease: 'easeOut' },
     },
   },
 });
