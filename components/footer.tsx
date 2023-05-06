5cdf1a9d8603ca875fcc9dbef05ac6e4761812b0
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-06 20:48:08 +0000

    feat: update footer content

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,21 @@
+import Link from 'next/link';
+
+import { siteInfo } from '@/config/site';
+
 export default function Footer() {
   return (
     <footer className="border-t bg-background dark:bg-background/10 dark:backdrop-blur-sm">
-      <div className="container flex items-center justify-center gap-2 py-3">
-        <p className="text-lg">&copy;</p>
-        <p>{new Date().getFullYear()}</p>
-        <p>GyBex</p>
+      <div className="container flex items-center justify-center gap-1 py-3">
+        <span>Built</span>
+        <span>by</span>
+        <Link
+          href={siteInfo.links.twitter}
+          target="_blank"
+          rel="noreferrer"
+          className="font-medium underline underline-offset-4"
+        >
+          GyBex
+        </Link>
       </div>
     </footer>
   );
