a6e3dbb2b0da40f89b9186929bdd586dcaacdb24
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-09 15:18:58 +0000

    feat: update providers

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,11 +1,17 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Provider as JotaiProvider } from 'jotai';
 import { ThemeProvider } from 'next-themes';
+
+const queryClient = new QueryClient();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" enableSystem defaultTheme="dark">
-      {children}
+      <QueryClientProvider client={queryClient}>
+        <JotaiProvider>{children}</JotaiProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
