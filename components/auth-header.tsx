d37557c8a3de419176658852c05ba655d7052b89
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-18 01:37:38 +0000

    feat: improve accessibility

--- components/auth-header.tsx
+++ components/auth-header.tsx
@@ -9,7 +9,7 @@ export default function AuthHeader() {
       <div className="container flex items-center justify-between gap-3 py-4 md:gap-5">
         <Link href="/" className={buttonVariants({ variant: 'ghost' })}>
           <>
-            <ChevronLeft className="mr-2 h-4 w-4" />
+            <ChevronLeft aria-hidden="true" className="mr-2 h-4 w-4" />
             Back
           </>
         </Link>
--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -18,8 +18,14 @@ export function ThemeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="sm" className="w-9 px-0">
-          <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 sm:h-6 sm:w-6" />
-          <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 sm:h-6 sm:w-6" />
+          <Sun
+            aria-hidden="true"
+            className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 sm:h-6 sm:w-6"
+          />
+          <Moon
+            aria-hidden="true"
+            className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 sm:h-6 sm:w-6"
+          />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -28,15 +34,15 @@ export function ThemeToggle() {
         className="bg-popover/90 backdrop-blur-sm"
       >
         <DropdownMenuItem onClick={() => setTheme('light')}>
-          <Sun className="mr-2 h-4 w-4" />
+          <Sun aria-hidden="true" className="mr-2 h-4 w-4" />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>
-          <Moon className="mr-2 h-4 w-4" />
+          <Moon aria-hidden="true" className="mr-2 h-4 w-4" />
           <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
-          <Laptop className="mr-2 h-4 w-4" />
+          <Laptop aria-hidden="true" className="mr-2 h-4 w-4" />
           <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
