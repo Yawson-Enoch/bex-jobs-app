aecc90f0bfc73b8a42c253be7855f5e2f201ae9c
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-20 17:00:53 +0000

    feat: update dashboard layout

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -7,10 +7,10 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="dashboard-grid-container relative min-h-screen">
-      <div className="dashboard-header h-fit w-full">
+      <div className="dashboard-header sticky top-0">
         <DashboardHeader />
       </div>
-      <div className="dashboard-sidebar h-screen w-60 border-r bg-background/90 backdrop-blur-sm"></div>
+      <div className="dashboard-sidebar sticky top-0 hidden max-h-screen w-60 overflow-y-scroll overscroll-y-contain border-r bg-background/90 backdrop-blur-sm md:block"></div>
       <main className="dashboard-main">{children}</main>
     </div>
   );
