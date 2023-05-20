86b3513228edd292b6fba2e5a014f25dcceac457
Yawson-Enoch <enochgybex25@gmail.com>
2023-05-20 15:50:52 +0000

    feat: add dashboard to robots disallow field

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/private/',
+      disallow: ['/private/', '/dashboard/'],
     },
     sitemap: 'https://acme.com/sitemap.xml',
   };
