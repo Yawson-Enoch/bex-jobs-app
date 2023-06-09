1bcf7f246216ce00cb3dd79918197baea1b04084
Yawson-Enoch <enochgybex25@gmail.com>
2023-06-09 17:19:38 +0000

    feat: debounce search input

--- src/components/common/all-jobs-search.tsx
+++ src/components/common/all-jobs-search.tsx
@@ -1,11 +1,20 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import { SearchIcon } from 'lucide-react';
 
+import useDebounce from '~/hooks/useDebounce';
 import useQueryParams from '~/hooks/useQueryParams';
 
 export default function AllJobsSearch() {
+  const [searchValue, setSearchValue] = useState('');
+
   const { setQueryParams } = useQueryParams();
+  const debouncedSearchValue = useDebounce(searchValue);
+
+  useEffect(() => {
+    setQueryParams({ search: debouncedSearchValue });
+  }, [debouncedSearchValue, setQueryParams]);
 
   return (
     <div className="group/search flex h-9 items-center gap-2 rounded-md border border-input p-1 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
@@ -17,7 +26,8 @@ export default function AllJobsSearch() {
         type="search"
         placeholder="Search..."
         className="h-full w-full bg-transparent placeholder:truncate placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
-        onChange={(e) => setQueryParams({ search: e.target.value })}
+        value={searchValue}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
     </div>
   );
