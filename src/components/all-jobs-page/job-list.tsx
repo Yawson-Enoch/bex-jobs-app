2e3cec6a3952348cc9afc41a42d7438e9afe6feb
Yawson-Enoch <enochgybex25@gmail.com>
2023-06-10 20:47:24 +0000

    feat: add jobs list view type ui

--- src/components/all-jobs-page/job-list.tsx
+++ src/components/all-jobs-page/job-list.tsx
@@ -2,10 +2,10 @@ import { BriefcaseIcon, CalendarDaysIcon, MapPinIcon } from 'lucide-react';
 
 import { Button } from '../ui/button';
 
-export default function JobGrid() {
+export default function JobList() {
   return (
-    <li className="space-y-3 rounded-lg border bg-slate-500/10 p-3 text-xs md:p-6 md:text-base">
-      <div className="space-y-3 border-b-4 border-dotted border-accent pb-3">
+    <li className="grid gap-3 rounded-lg border bg-slate-500/10 p-3 text-xs md:grid-cols-[auto,_2fr,1fr] md:justify-between md:gap-6 md:p-6 md:text-base">
+      <div className="space-y-3 border-b-4 border-dotted border-accent pb-3 md:border-b-0 md:border-r-4 md:pb-0 md:pr-6">
         <p className="rounded-md bg-primary py-1 text-center text-5xl font-bold text-primary-foreground">
           G
         </p>
@@ -17,20 +17,22 @@ export default function JobGrid() {
         </div>
       </div>
 
-      <div className="space-y-3">
-        <div className="flex items-center gap-1">
-          <MapPinIcon size={15} />
-          <span className="truncate">Location</span>
-        </div>
-        <div className="flex items-center gap-1">
-          <CalendarDaysIcon size={15} />
-          <span className="truncate">10th June 2010</span>
+      <div className="space-y-3 md:space-y-6 md:justify-self-center">
+        <div className="flex flex-wrap items-center justify-between gap-3 md:gap-6">
+          <div className="flex items-center gap-1">
+            <MapPinIcon size={15} />
+            <span>Location</span>
+          </div>
+          <div className="flex items-center gap-1">
+            <CalendarDaysIcon size={15} />
+            <span>10th June 2010</span>
+          </div>
         </div>
 
         <div className="flex flex-wrap items-center justify-between gap-3 md:gap-6">
           <div className="flex items-center gap-1">
             <BriefcaseIcon size={15} />
-            <span className="truncate">Type</span>
+            <span>Type</span>
           </div>
           <span className="rounded-md bg-blue-500/20 px-2.5 py-0.5 text-xs font-medium text-blue-500 dark:bg-blue-500/10">
             Status
@@ -38,7 +40,7 @@ export default function JobGrid() {
         </div>
       </div>
 
-      <div className="grid gap-3 md:grid-cols-2">
+      <div className="grid grid-cols-2 gap-3 md:grid-cols-1">
         <Button className="bg-yellow-500 text-[rgb(248,_250,_252)] dark:bg-yellow-700 [&:not(:disabled)]:hover:bg-yellow-500/90 [&:not(:disabled)]:dark:hover:bg-yellow-700/90">
           Edit
         </Button>
--- src/components/all-jobs-page/jobs.tsx
+++ src/components/all-jobs-page/jobs.tsx
@@ -1,21 +1,22 @@
 import JobGrid from './job-grid';
+import JobList from './job-list';
 
 export default function Jobs() {
   return (
     <section className="space-y-6">
       <h4>30 Jobs Found</h4>
 
-      <ul className="grid grid-cols-[repeat(auto-fit,_minmax(min(15rem,_100%),_1fr))] gap-3 md:gap-6">
+      <ul className="grid grid-cols-2 gap-3 md:grid-cols-3 md:gap-6 lg:grid-cols-4">
         <JobGrid />
         <JobGrid />
         <JobGrid />
         <JobGrid />
-        <JobGrid />
-        <JobGrid />
-        <JobGrid />
-        <JobGrid />
-        <JobGrid />
-        <JobGrid />
+      </ul>
+      <ul className="space-y-3 md:space-y-6">
+        <JobList />
+        <JobList />
+        <JobList />
+        <JobList />
       </ul>
     </section>
   );
