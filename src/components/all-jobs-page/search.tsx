d322a26677c1d100c474974db246394ce023a0c1
Yawson-Enoch <enochgybex25@gmail.com>
2023-06-23 21:17:26 +0000

    feat: add clr search term btn

--- src/components/all-jobs-page/search.tsx
+++ src/components/all-jobs-page/search.tsx
@@ -1,7 +1,8 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { SearchIcon } from 'lucide-react';
+import { SearchIcon, XIcon } from 'lucide-react';
+import { twMerge } from 'tailwind-merge';
 
 import useDebounce from '~/hooks/useDebounce';
 import useQueryParams from '~/hooks/useQueryParams';
@@ -13,7 +14,7 @@ export default function Search() {
   const debouncedSearchValue = useDebounce(searchValue);
 
   useEffect(() => {
-    if (debouncedSearchValue.trim() === '') {
+    if (debouncedSearchValue === '') {
       deleteQueryParam('search');
     } else {
       setQueryParams({ search: debouncedSearchValue });
@@ -21,7 +22,7 @@ export default function Search() {
   }, [debouncedSearchValue, deleteQueryParam, setQueryParams]);
 
   return (
-    <div className="group/search flex h-9 items-center gap-2 rounded-md bg-muted px-2 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
+    <div className="group/search grid h-9 grid-cols-[auto,_1fr,_auto] items-center gap-2 rounded-md bg-muted pl-2 pr-1 text-sm ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2">
       <SearchIcon
         size={20}
         className="text-muted-foreground group-focus-within/search:text-foreground"
@@ -29,10 +30,19 @@ export default function Search() {
       <input
         type="search"
         placeholder="Search job..."
-        className="h-full w-full bg-transparent placeholder:truncate placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
+        className="bg-transparent placeholder:truncate placeholder:text-muted-foreground focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50"
         value={searchValue}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={(e) => setSearchValue(e.target.value.trim())}
       />
+      <button
+        className={twMerge(
+          'pointer-events-none invisible rounded-full border-2 border-transparent p-0.5 focus-visible:border-background focus-visible:outline-none active:border-background',
+          searchValue !== '' && 'pointer-events-auto visible'
+        )}
+        onClick={() => setSearchValue('')}
+      >
+        <XIcon size={17} />
+      </button>
     </div>
   );
 }
