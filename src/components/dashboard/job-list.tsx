70a1e230b579a122945a744f8fc09649654be43d
Yawson-Enoch <enochgybex25@gmail.com>
2023-07-09 23:48:35 +0000

    refactor: remove `misc` file

--- src/components/dashboard/job-list.tsx
+++ src/components/dashboard/job-list.tsx
@@ -3,10 +3,15 @@ import { BriefcaseIcon, CalendarDaysIcon, MapPinIcon } from 'lucide-react';
 import { twMerge } from 'tailwind-merge';
 
 import { TJobAPI } from '~/schemas/job';
-import { jobStatusColors } from '~/lib/misc';
 
 import DeleteJobBtn from './delete-job-btn';
 import EditJobBtn from './edit-job-btn';
+
+const jobStatusColors = {
+  pending: 'bg-yellow-500/20 text-yellow-500 dark:bg-yellow-500/10',
+  interview: 'bg-blue-500/20 text-blue-500 dark:bg-blue-500/10',
+  declined: 'bg-red-500/20 text-red-500 dark:bg-red-500/10',
+};
 
 export default function JobList({ job }: { job: TJobAPI }) {
   const date = new Date(job.createdAt);
--- src/lib/misc.ts
+++
@@ -1,5 +0,0 @@
-export const jobStatusColors = {
-  pending: 'bg-yellow-500/20 text-yellow-500 dark:bg-yellow-500/10',
-  interview: 'bg-blue-500/20 text-blue-500 dark:bg-blue-500/10',
-  declined: 'bg-red-500/20 text-red-500 dark:bg-red-500/10',
-};
