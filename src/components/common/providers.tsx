0dbfb55f8e418e30d1f05a592fcd0e53cfeb5dd8
Yawson-Enoch <enochgybex25@gmail.com>
2023-08-05 20:32:04 +0000

    fix: `Detected multiple Jotai instances` warning

--- src/components/common/providers.tsx
+++ src/components/common/providers.tsx
@@ -2,16 +2,17 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { Provider as JotaiProvider } from 'jotai';
+import { Provider as JotaiProvider, createStore } from 'jotai';
 import { ThemeProvider } from 'next-themes';
 
 const queryClient = new QueryClient();
+const store = createStore();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" enableSystem defaultTheme="dark">
       <QueryClientProvider client={queryClient}>
-        <JotaiProvider>{children}</JotaiProvider>
+        <JotaiProvider store={store}>{children}</JotaiProvider>
         <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
       </QueryClientProvider>
     </ThemeProvider>
