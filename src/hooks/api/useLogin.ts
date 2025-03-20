1c301d5799794a9bdaa22beb99bae0584ab8e980
Yawson-Enoch <enochgybex25@gmail.com>
2025-03-20 17:27:55 +0000

    feat: add auto login on signup

--- src/hooks/api/useLogin.ts
+++ src/hooks/api/useLogin.ts
@@ -24,7 +24,7 @@ export function useLogin() {
       toast({
         description: data.msg,
       });
-      router.push('/dashboard');
+      router.replace('/dashboard');
     },
   });
 }
--- src/hooks/api/useSignup.ts
+++ src/hooks/api/useSignup.ts
@@ -1,25 +1,30 @@
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
+import { useSetAtom } from 'jotai';
 
-import { ApiMessage, Signup } from '~/schemas/auth';
+import { ApiLogin, Signup } from '~/schemas/auth';
 import { apiClient } from '~/lib/axios-instance';
+import { accessTokenAtom } from '~/atoms/auth-token';
 import { toast } from '~/components/ui/use-toast';
 
 const signupUser = async (payload: Signup) => {
   const response = await apiClient.post(`/auth/register`, payload);
 
-  return ApiMessage.parse(response.data);
+  return ApiLogin.parse(response.data);
 };
 export function useSignup() {
+  const setAccessToken = useSetAtom(accessTokenAtom);
+
   const router = useRouter();
 
   return useMutation({
     mutationFn: signupUser,
     onSuccess: (data) => {
+      setAccessToken(data.data.token);
       toast({
         description: data.msg,
       });
-      router.push('/login');
+      router.replace('/dashboard');
     },
   });
 }
